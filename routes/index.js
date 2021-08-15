1a4e08ee79e328cf4efd697df8e388e2087b6437
Stin <krikarlss@gmail.com>
2021-08-16 00:25:41 +0200

    Get data from db

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,33 @@
 const express = require('express');
 const router = express.Router();
+const mongodb = require('mongodb');
+
+// Setup MongoDB
+const MongoClient = mongodb.MongoClient;
+const dbURL = "mongodb://localhost";
 
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Chart' });
+
 });
+
+router.get('/price-data', function(req, res, next) {
+
+  MongoClient.connect(dbURL, { useNewUrlParser: true, useUnifiedTopology: true }, (err, client) => {
+    if (err) throw err;
+    else {
+      const db = client.db('rainbow');
+      const prices = db.collection('prices');
+
+      prices.find().project({}).toArray( (err, docs) => {
+        if (err) throw err;
+        else { res.send(docs); }
+        client.close();
+      });
+    }
+  });
+});
+
+
 
 module.exports = router;
