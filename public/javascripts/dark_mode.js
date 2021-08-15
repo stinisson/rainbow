4ed41d8a323ed311023166a1a0b3ca25ac4f6662
Stin <krikarlss@gmail.com>
2021-08-15 19:17:59 +0200

    Add sample chart and dark mode toggle

---
+++ public/javascripts/dark_mode.js
@@ -0,0 +1,44 @@
+
+
+
+$(document).ready(() => {
+
+  const options = {
+    bottom: '64px', // default: '32px'
+    right: 'unset', // default: '32px'
+    left: '32px', // default: 'unset'
+    time: '0.5s', // default: '0.3s'
+    mixColor: '#fff', // default: '#fff'
+    backgroundColor: '#d5d5d5',  // default: '#fff'
+    buttonColorDark: '#212121',  // default: '#100f2c' 1A1A1A
+
+    buttonColorLight: '#d5d5d5', // default: '#fff'
+    saveInCookies: false, // default: true,
+    //label: '🌙', // default: ''
+    label: '<i id="dark-mode-icon" class="fas fa-moon"></i>', // default: ''
+    autoMatchOsTheme: true // default: true
+  }
+
+
+  const darkmode = new Darkmode(options);
+  darkmode.showWidget();
+
+  $( ":button.darkmode-toggle" ).click(function() {
+    console.log("Handler for .click() called") // will return true
+
+    if(darkmode.isActivated()) {
+      $("#dark-mode-icon").removeClass("fa-moon");
+      $("#dark-mode-icon").addClass("fa-sun");
+      $("#dark-mode-icon").css("color", "#2A2A2A");
+    } else {
+      $("#dark-mode-icon").css("color", "#fff");
+      $("#dark-mode-icon").removeClass("fa-sun");
+      $("#dark-mode-icon").addClass("fa-moon");
+    }
+  });
+
+  console.log(darkmode.isActivated()) // will return true
+
+
+
+});
