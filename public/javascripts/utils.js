5a0b3638063c94eddfc004e1308107019182f535
Stin <krikarlss@gmail.com>
2021-08-16 19:59:51 +0200

    Update colours

--- public/javascripts/utils.js
+++ public/javascripts/utils.js
@@ -4,6 +4,17 @@ export const CHART_COLORS = {
   yellow: 'rgb(255, 205, 86)',
   green: 'rgb(75, 192, 192)',
   blue: 'rgb(54, 162, 235)',
+  indigo: 'rgb(5, 93, 161)',
   purple: 'rgb(153, 102, 255)',
-  grey: 'rgb(201, 203, 207)'
+  grey: 'rgb(255,255,255)'
+};
+
+export const CHART_COLORS_TRANSPARENT = {
+  red: 'rgba(255, 99, 132, 0.9)',
+  orange: 'rgba(255, 159, 64, 0.9)',
+  yellow: 'rgba(255, 205, 86, 0.9)',
+  green: 'rgba(75, 192, 192, 0.9)',
+  blue: 'rgba(54, 162, 235, 0.9)',
+  indigo: 'rgba(5, 93, 161, 0.9)',
+  purple: 'rgba(153, 102, 255, 0.9)'
 };
