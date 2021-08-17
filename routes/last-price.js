d2b4654b415afd9da944d20756c6a6ba183a0acc
Stin <krikarlss@gmail.com>
2021-08-17 20:23:14 +0200

    Get last price via stream and save it once a day to db

---
+++ routes/last-price.js
@@ -0,0 +1,60 @@
+const express = require('express');
+const router = express.Router();
+const mongodb = require('mongodb');
+const CronJob = require('cron').CronJob;
+
+// Setup MongoDB
+const MongoClient = mongodb.MongoClient;
+const dbURL = "mongodb://localhost";
+
+let lastPrice = 0;
+function saveLastPrice() {
+
+  const priceDocument = {"price": lastPrice, "timestamp": parseInt(Date.now()/1000)}   ;
+
+  MongoClient.connect(dbURL, { useNewUrlParser: true, useUnifiedTopology: true }, (err, client) => {
+    if (err) throw err;
+    else {
+      const db = client.db('rainbow');
+      db.collection("prices").insertOne(priceDocument, function(err, res) {
+        if (err) throw err;
+        console.log("Saved last price");
+      })
+    }
+  });
+
+}
+
+const {BitstampStream, CURRENCY} = require("node-bitstamp");
+
+/* STREAMS */
+// @ https://www.bitstamp.net/websocket/v2/
+
+// Live trades
+const bitstampStream = new BitstampStream();
+
+
+bitstampStream.on("connected", () => {
+  const btcUsdTickerChannel = bitstampStream.subscribe(bitstampStream.CHANNEL_LIVE_TRADES, CURRENCY.BTC_USD);
+  bitstampStream.on(btcUsdTickerChannel, ({ data, event }) => {
+    console.log(data.price);
+    lastPrice = data.price;
+
+  });
+});
+
+bitstampStream.on("disconnected", () => {});
+
+
+router.get('/',function(req, res, next) {
+  res.json({"lastPrice": lastPrice});
+});
+
+module.exports = router;
+
+
+// Update opening hours once every five minutes
+// cronTime, onTick, onComplete, start, timezone, context, runOnInit
+// Save last price five seconds past midnight everyday
+const job = new CronJob('*/10 * * * * *', saveLastPrice, null, null, null, null, false);
+job.start();
