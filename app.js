964b55268d803ab50a402475f766f7a05d321495
Stin <krikarlss@gmail.com>
2021-08-18 11:56:31 +0200

    Update 404 error page message

--- app.js
+++ app.js
@@ -33,7 +33,7 @@ app.use('/about', aboutRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  next(createError(404));
+  next(createError(404, "404 - Uncharted territory"));
 });
 
 // error handler
